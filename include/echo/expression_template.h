de5b05ff83306763d63c5139ab1b955b959bc014
Ribulous Barnulous <ribulous.barnulous@gmail.com>
2014-12-08 08:13:20 -0500

    added exporting header

---
+++ include/echo/expression_template.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <echo/expression_template/expression_template_assignment.h>
+#include <echo/expression_template/arithmetic.h>
+#include <echo/expression_template/map.h>
+#include <echo/expression_template/reduce.h>
+#include <echo/expression_template/math.h>
+
+namespace echo {
+
+using expression_template::ExpressionTemplateAssignment;
+using expression_template::ExpressionTemplateConstAssignment;
+
+} //end namespace echo
